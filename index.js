97a1c35638ad373dad2826021a7de9765a01732d
Ste2038 <giulianelliste@gmail.com>
2018-03-29 11:30:28 +0200

    prima commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+import { constants } from 'perf_hooks';
+
 var socket = require('socket.io-client')('https://socketdialogflow.herokuapp.com/');
 var SerialPort = require('serialport');
-/*var port = new SerialPort('COM3', {
-  baudRate: 9600
-});*/
+
 var fs = require('fs');
 var TelegramBot = require('node-telegram-bot-api');
 var Constants = require('./My files/constants');
@@ -12,12 +12,12 @@ const option = {mode: 0o600};
 const token = Constants.Token;
 const bot = new TelegramBot(token, {polling: true});
 */
+var port = new SerialPort(Constants.ComName, {
+  baudRate: Constants.ComBound
+});
+
 let AutorizedId = [];
-const ExStrategyBotDataPath = "ExtrategyBotData.json";
+const ExStrategyBotDataPath = "./My files/ExtrategyBotData.json";
 const credenziali = {};
 credenziali[Constants.HeadersKey] = Constants.HeadersValue;
 const MasterID = Constants.AutorizedMasterID;
-
-AutorizedId [0, 1] = "ciao";
-AutorizedId [1, 0] = "no";
-console.log(AutorizedId);
